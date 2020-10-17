48ebb161397767be0b941ed505f5c9c1ed91f981
amitsinghnegi <66195521+amitsinghnegi@users.noreply.github.com>
2020-10-17 08:07:06 +0530

    Create MergeSort.java

---
+++ java/Sorting/MergeSort.java
@@ -0,0 +1,39 @@
+package sorting;
+
+import java.io.*;
+import java.util.stream.Stream;
+
+public class MergeSort {
+
+	private static void partition(int[] a, int l, int r) {
+		if (l >= r) return;
+    
+		int mid = (l + r) / 2;
+		partition(a, l, mid);
+		partition(a, mid + 1, r);
+		merge(a, l, mid, r);
+	}
+
+	private static void merge(int[] a, int l, int mid, int r) {
+		int temp[] = new int[r - l + 1];
+		int k = 0, i = l, j = mid + 1;
+
+		while (i <= mid && j <= r)
+			temp[k++] = (a[i] <= a[j]) ? a[i++] : a[j++];
+
+		while (i <= mid)
+			temp[k++] = a[i++];
+		while (j <= r)
+			temp[k++] = a[j++];
+
+		for (int t = l; t <= r; t++)
+			a[t] = temp[t - l];
+	}
+
+	public static void main(String[] args) {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = Stream.of(br.readLine().replaceAll("\\s+", " ").split(" ")).mapToInt(Integer::parseInt).toArray();
+		partition(a, 0, a.length - 1);
+		System.out.println(Arrays.toString(a));
+	}
+}
